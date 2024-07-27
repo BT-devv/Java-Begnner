7c82a2d07b7b2a185da4ac32118c8578b071a4bb
BT-devv <buitrungtuan2003@gmail.com>
2024-07-27 22:10:28 +0700

    first commit

---
+++ src/tuanbui/HelloWorld.java
@@ -0,0 +1,15 @@
+package tuanbui;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello world with java");
+		System.out.println("Hello world with java");
+		System.out.println("Hello world with java");
+		System.out.println("Hello world with java");
+		System.out.println("Hello world with java");
+		System.out.println("Hello world with java");
+		System.out.println("Hello world with java");
+		System.out.println("Hello world with java");
+		System.out.println("Hello world with java");
+	}
+}
